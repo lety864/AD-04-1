cdfaa6f31f0184984a320cc161c85a2103035a0b
Leticia Janet BR <bermudez.leticia.2609@gmail.com>
2025-09-18 19:32:44 -0600

    Added project files

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+// alert("Hello this is an alert");
+
+
+function alertFunction() {
+    alert("Es hora de una hamburguesa"); 
+}
+
+function logFunction() {
+    console.log("Alguien hizo click en Burger Town");
+}
+
+function changeColor() {
+    const changeColorRed = document.getElementById("red");
+    changeColorRed.style.color = "red";
+}
